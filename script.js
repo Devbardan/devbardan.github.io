258387dd1c4215000e03cea40de5355f75321b3a
Bardev <67128253+bardeveloop@users.noreply.github.com>
2024-04-12 20:13:31 +0200

    add  dark mode

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
 const navLinkEls = document.querySelectorAll('.nav_link');
 const sectionEls = document.querySelectorAll('.sections');
 const labelEls = document.querySelectorAll('.nav-label');
+const toggleButton = document.getElementById("toggle-button");
+const toogles = document.querySelectorAll('.toogle');
+const body = document.querySelector('body');
 
-// Agrega un evento de hover a cada .nav_link
+function loadDarksMode(){
+  const darksModeOn = localStorage.getItem('darksOn') === 'true';
+    if (darksModeOn) {
+      body.classList.add('darks');
+    }
+}
+function loadLigthMode(){
+  const ligthModeOn = localStorage.getItem('darksOn') === 'false';
+  if (!ligthModeOn){
+    body.classList.remove('darks');
+  }
+}
+
+toggleButton.onclick = function() {
+  toogles.forEach(toggle => {
+    toggle.classList.toggle('hidden');
+    let darksOn = toggle.classList.contains('hidden');
+    localStorage.setItem('darksOn', darksOn);
+  });
+  document.body.classList.toggle('darks');
+};
+
+
 navLinkEls.forEach((navLinkEl, index) => {
   navLinkEl.addEventListener('mouseenter', () => {
-    labelEls[index].classList.add('visible'); // Mostrar el texto correspondiente al hacer hover
+    labelEls[index].classList.add('visible'); 
   });
 
   navLinkEl.addEventListener('mouseleave', () => {
-    labelEls[index].classList.remove('visible'); // Ocultar el texto correspondiente al salir del hover
+    labelEls[index].classList.remove('visible');
   });
 });
 let labelTimeout;
 let currentSection = 'home';
 
 window.addEventListener('scroll', () => {
-  const windowHeight = window.innerHeight; // Altura de la ventana del navegador
-  const middleScreen = windowHeight / 2; // Posición vertical del centro de la pantalla
+  const windowHeight = window.innerHeight; 
+  const middleScreen = windowHeight / 2; 
 
   sectionEls.forEach(sectionEl => {
-    const sectionTop = sectionEl.getBoundingClientRect().top; // Cambio aquí
+    const sectionTop = sectionEl.getBoundingClientRect().top; 
     const sectionBottom = sectionTop + sectionEl.offsetHeight;
 
-    // Verifica si la sección está cerca del centro de la pantalla
+   
     if (sectionTop <= middleScreen && sectionBottom >= middleScreen) {
       currentSection = sectionEl.id;
     }
@@ -32,14 +57,15 @@ window.addEventListener('scroll', () => {
   navLinkEls.forEach((navLinkEl, index) => {
     if (navLinkEl.href.endsWith(`#${currentSection}`)) {
       navLinkEl.classList.add('active');
-      labelEls[index].classList.add('visible'); // Mostrar el texto correspondiente
+      labelEls[index].classList.add('visible'); 
       clearTimeout(labelTimeout);
       labelTimeout = setTimeout(() => {
         labelEls[index].classList.remove('visible');
-      }, 2000); // 2000 milisegundos equivalen a 2 segundos
+      }, 2000); 
     } else {
       navLinkEl.classList.remove('active');
-      labelEls[index].classList.remove('visible'); // Ocultar el texto correspondiente
+      labelEls[index].classList.remove('visible'); 
     }
   });
 });
+loadDarksMode();
